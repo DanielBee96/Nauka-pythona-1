4c4b4ec86ff2fe782093189e988c1ba421031690
DanielBee96 <beezydaniel96@gmail.com>
2026-06-20 09:21:19 +0200

    Create sesja-3.py

---
+++ sesja-3.py
@@ -0,0 +1,11 @@
+# Sesja 3 - if else
+wynik = 85
+
+print("Mój wynik z testu:", wynik)
+
+if wynik >= 90:
+    print("Ocena: 5. Szacun!")
+elif wynik >= 70:
+    print("Ocena: 4. Dobrze Ci idzie.")
+else:
+    print("Ocena: 3. Trzeba docisnąć.")
