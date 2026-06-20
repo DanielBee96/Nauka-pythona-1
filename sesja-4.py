ac4712eee208de6ab3d51d32eb617aea160bc0d3
DanielBee96 <beezydaniel96@gmail.com>
2026-06-20 09:30:01 +0200

    Create sesja-4.py

---
+++ sesja-4.py
@@ -0,0 +1,14 @@
+# Sesja 4 - funkcje def
+def ocen_test(wynik):
+    if wynik >= 90:
+        print("Ocena: 5. Szacun!")
+    elif wynik >= 70:
+        print("Ocena: 4. Dobrze Ci idzie.")
+    else:
+        print("Ocena: 3. Trzeba docisnąć.")
+
+# Używamy funkcji 3 razy
+print("Sprawdzam wyniki:")
+ocen_test(95)  # pierwszy uczeń
+ocen_test(82)  # drugi uczeń
+ocen_test(60)  # trzeci uczeń
